038dd5ba0d4b750f52e428b518dc1f3243b7ff11
Jason Wu <j.wu@xilinx.com>
2014-04-05 22:27:01 +1000

    slcrps: Update to match 3.13 kernel dts
    - correct clock sub node structure
    - Add comp str syscon
    - add ranges
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- slcrps/data/slcrps.tcl
+++ slcrps/data/slcrps.tcl
@@ -1,16 +1,14 @@
 
 proc generate {drv_handle} {
     gen_clocks_node $drv_handle
+
 }
 
 proc gen_clocks_node { drv_handle} {
-    set clocks_child_name "clocks"
+    set clocks_child_name "clkc"
     set clocks [hsm::utils::add_new_child_node $drv_handle $clocks_child_name]
-    hsm::utils::add_new_property $clocks "#address-cells" int 1
-    hsm::utils::add_new_property $clocks "#size-cells" int 0
-    set clkc [hsm::utils::add_new_child_node $clocks "clkc:clkc"]
-    hsm::utils::add_new_property $clkc "#clock-cells" int 1
-    hsm::utils::add_new_property $clkc clock-output-names stringlist  { armpll ddrpll iopll cpu_6or4x \
+    hsm::utils::add_new_property $clocks "#clock-cells" int 1
+    hsm::utils::add_new_property $clocks clock-output-names stringlist  { armpll ddrpll iopll cpu_6or4x \
 									cpu_3or2x cpu_2x cpu_1x ddr2x ddr3x \
 									dci lqspi smc pcap gem0 gem1 \
 									fclk0 fclk1 fclk2 fclk3 can0 can1 \
@@ -20,10 +18,11 @@ proc gen_clocks_node { drv_handle} {
 									spi0_aper spi1_aper can0_aper can1_aper \
 									i2c0_aper i2c1_aper uart0_aper uart1_aper \
 									gpio_aper lqspi_aper smc_aper swdt \
-									dbg_trc } 
-    hsm::utils::add_new_property $clkc compatible stringlist "xlnx,ps7-clkc"
-    hsm::utils::add_new_property $clkc fclk-enable int 0xf
-    hsm::utils::add_new_property $clkc ps-clk-frequency int 33333333
+									dbg_trc dbg_apb}
+    hsm::utils::add_new_property $clocks compatible stringlist "xlnx,ps7-clkc"
+    hsm::utils::add_new_property $clocks fclk-enable int 0xf
+    hsm::utils::add_new_property $clocks ps-clk-frequency int 33333333
+    # FIXME: quick hack to get kernel booting
+    hsm::utils::add_new_property $clocks reg hex "0x100 0x100"
     return $clocks
 }
-
