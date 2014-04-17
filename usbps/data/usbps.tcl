139841a6440bc9a948ccbd7e1c24d94c637a7754
Jason Wu <j.wu@xilinx.com>
2014-04-17 13:07:05 +1000

    usbps: Set up usb-reset correctly for empty usb reset
     - remove "<Select>" for C_USB_RESET value
     - replace "MIO" or "MIO " with empty string
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- usbps/data/usbps.tcl
+++ usbps/data/usbps.tcl
@@ -1,11 +1,17 @@
-proc generate {drv_handle} {
+proc ps7_reset_handle {drv_handle reset_pram conf_prop} {
     set ip [get_cells $drv_handle]
-    set value [get_property CONFIG.C_USB_RESET $ip]
+    set value [get_property CONFIG.${reset_pram} $ip]
+    # workaround for reset not been selected and show as "<Select>"
+    regsub -all "<Select>" $value "" value
     if { [llength $value] } {
-        regsub -all "MIO" $value "" value
+        regsub -all "MIO( |)" $value "" value
         if { $value != "-1" && [llength $value] !=0  } {
-            set_property CONFIG.usb-reset "ps7_gpio_0 $value 0" $drv_handle
+            set_property CONFIG.${conf_prop} "ps7_gpio_0 $value 0" $drv_handle
         }
-    }  
+    }
+}
+
+proc generate {drv_handle} {
+    ps7_reset_handle $drv_handle C_USB_RESET usb-reset
 }
 
