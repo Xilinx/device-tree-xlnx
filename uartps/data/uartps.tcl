b9e279155899111f3833b8f3425957451322044a
Jason Wu <j.wu@xilinx.com>
2014-04-28 11:56:30 +1000

    usrtps: Remove extra space before {
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- uartps/data/uartps.tcl
+++ uartps/data/uartps.tcl
@@ -3,7 +3,7 @@ proc generate {drv_handle} {
 
     set consoleip [get_property CONFIG.console_device [get_os] ]
     set port_number 0
-    if { [string match -nocase "$ip" "$consoleip"]  == 0 }  {
+    if { [string match -nocase "$ip" "$consoleip"]  == 0 } {
         set serial_count [hsm::utils::get_os_parameter_value "serial_count"]
         if { [llength $serial_count]  == 0 } {
             set serial_count 0
