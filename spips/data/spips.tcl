2e7f45002e228164884b506d5b40f4ffd2b5cab1
Jason Wu <j.wu@xilinx.com>
2014-05-07 14:13:02 +1000

    spips: Add num-cs and is-decoded-cs binding
    - is-decoded-cs is hard coded to 0
    - num-cs is determine based on the CONFIG.C_HAS_SS[0-2] value
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- spips/data/spips.tcl
+++ spips/data/spips.tcl
@@ -1,2 +1,11 @@
 proc generate {drv_handle} {
+	set ip [get_cells $drv_handle]
+	# SPI PS only have chip select range 0 - 2
+	foreach n {0 1 2} {
+		set cs_en [get_property CONFIG.C_HAS_SS${n} $ip]
+		if {[string equal "1" $cs_en]} {
+			inc cs-num
+		}
+	}
+	set_property CONFIG.num-cs ${cs-num} $drv_handle
 }
