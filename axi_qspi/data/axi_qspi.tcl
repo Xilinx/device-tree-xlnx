da66de35577233a46b9c72d9ade00edf192bab45
Jason Wu <j.wu@xilinx.com>
2014-06-06 15:35:24 +1000

    axi_qspi: Do not generate ip parameters
    except generate xlnx,num-ss-bit property
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- axi_qspi/data/axi_qspi.tcl
+++ axi_qspi/data/axi_qspi.tcl
@@ -1,3 +1,13 @@
 proc generate {drv_handle} {
-}
+	# try to source the common tcl procs
+	# assuming the order of return is based on repo priority
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
 
+	set_drv_conf_prop $drv_handle "C_NUM_SS_BITS" "xlnx,num-ss-bits"
+}
