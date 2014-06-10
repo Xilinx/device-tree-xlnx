e61c09f5ba97bdc40dbd95f9228f3743aad30177
Jason Wu <j.wu@xilinx.com>
2014-06-10 13:34:06 +1000

    dt:common_proc:set_drv_conf_prop: Add property type configuration
     - Allow user to to set property type
     - Add boolean property
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- device_tree/data/common_proc.tcl
+++ device_tree/data/common_proc.tcl
@@ -10,14 +10,36 @@ proc get_clock_frequency {ip_handle portname} {
 	return $clk
 }
 
-proc set_drv_conf_prop {drv_handle pram conf_prop} {
+proc set_drv_conf_prop args {
+	set drv_handle [lindex $args 0]
+	set pram [lindex $args 1]
+	set conf_prop [lindex $args 2]
 	set ip [get_cells $drv_handle]
 	set value [get_property CONFIG.${pram} $ip]
 	if { [llength $value] } {
 		regsub -all "MIO( |)" $value "" value
 		if { $value != "-1" && [llength $value] !=0  } {
-			set_property CONFIG.${conf_prop} $value $drv_handle
+			if {[llength $args] >= 4} {
+				set type [lindex $args 3]
+				if {[string equal -nocase $type "boolean"]} {
+					set_boolean_property $drv_handle $value ${conf_prop}
+					return 0
+				}
+				set_property ${conf_prop} $value $drv_handle
+				set prop [get_comp_params ${conf_prop} $drv_handle]
+				set_property CONFIG.TYPE $type $prop
+				return 0
+			}
+			set_property ${conf_prop} $value $drv_handle
 		}
+	}
+}
+
+proc set_boolean_property {drv_handle value conf_prop} {
+	if {[expr $value >= 1]} {
+		set_property ${conf_prop} "" $drv_handle
+		set prop [get_comp_params ${conf_prop} $drv_handle]
+		set_property CONFIG.TYPE referencelist $prop
 	}
 }
 
