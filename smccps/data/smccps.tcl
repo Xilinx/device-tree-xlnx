d850276b57c01be4f673c9333e77d2115682b17a
Jason Wu <j.wu@xilinx.com>
2014-06-16 11:32:54 +1000

    smccps: Remove unnecessary binding properties
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- smccps/data/smccps.tcl
+++ smccps/data/smccps.tcl
@@ -1,18 +1,2 @@
-proc set_drv_conf_prop {drv_handle pram conf_prop} {
-    set ip [get_cells $drv_handle]
-    set value [get_property CONFIG.${pram} $ip]
-    if { [llength $value] } {
-        regsub -all "MIO( |)" $value "" value
-        if { $value != "-1" && [llength $value] !=0  } {
-            set_property CONFIG.${conf_prop} $value $drv_handle
-        }
-    }
-}
-
 proc generate {drv_handle} {
-    set_drv_conf_prop $drv_handle "C_ADDR25" "arm,addr25"
-    set_drv_conf_prop $drv_handle "C_NOR_CHIP_SEL0" "arm,nor-chip-sel0"
-    set_drv_conf_prop $drv_handle "C_NOR_CHIP_SEL1" "arm,nor-chip-sel1"
-    set_drv_conf_prop $drv_handle "C_SRAM_CHIP_SEL0" "arm,sram-chip-sel0"
-    set_drv_conf_prop $drv_handle "C_SRAM_CHIP_SEL1" "arm,sram-chip-sel1"
 }
