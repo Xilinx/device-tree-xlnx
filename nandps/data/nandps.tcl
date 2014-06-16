3516301455cf32f47a48b242a9ff1315c8ced1d6
Jason Wu <j.wu@xilinx.com>
2014-06-16 11:35:01 +1000

    nandps: Set arm,nand-cycle-tX properties based on C_NAND_CYCLE_TX
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- nandps/data/nandps.tcl
+++ nandps/data/nandps.tcl
@@ -1,16 +1,19 @@
-proc set_drv_conf_prop {drv_handle pram conf_prop} {
-    set ip [get_cells $drv_handle]
-    set value [get_property CONFIG.${pram} $ip]
-    #puts $value
-    if { [llength $value] } {
-        regsub -all "MIO( |)" $value "" value
-        if { $value != "-1" && [llength $value] !=0  } {
-            set_property CONFIG.${conf_prop} $value $drv_handle
+proc generate {drv_handle} {
+    # try to source the common tcl procs
+    # assuming the order of return is based on repo priority
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
         }
     }
-}
 
-proc generate {drv_handle} {
-    set_drv_conf_prop $drv_handle C_NAND_WIDTH nand-bus-width
-    # FIXME: once the timing data is exported by Vivado. We should update the timing data
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T0" "arm,nand-cycle-t0"
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T1" "arm,nand-cycle-t1"
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T2" "arm,nand-cycle-t2"
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T3" "arm,nand-cycle-t3"
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T4" "arm,nand-cycle-t4"
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T5" "arm,nand-cycle-t5"
+    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T6" "arm,nand-cycle-t6"
 }
