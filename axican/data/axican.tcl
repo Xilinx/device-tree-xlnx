0f34dbef425b976ed0c3d3ed6f15fb7847f2bbd7
Jason Wu <j.wu@xilinx.com>
2014-06-30 14:04:18 +1000

    axican: Setup tx and rx depth binding
    Set up tx and rx depth bindings
     - xlnx,can-tx-dpth based on c_can_tx_dpth
     - xlnx,can-rx-dpth based on c_can_rx_dpth
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- axican/data/axican.tcl
+++ axican/data/axican.tcl
@@ -1,2 +1,13 @@
 proc generate {drv_handle} {
+    # try to source the common tcl procs
+    # assuming the order of return is based on repo priority
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
+    }
+    set_drv_conf_prop $drv_handle c_can_tx_dpth xlnx,can-tx-dpth hexint
+    set_drv_conf_prop $drv_handle c_can_rx_dpth xlnx,can-rx-dpth hexint
 }
