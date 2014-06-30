92539f7b7a15c356744c4a7dcbdfde9d2304aa59
Jason Wu <j.wu@xilinx.com>
2014-06-30 14:06:48 +1000

    axi_traffic_gen: Fix ATG support
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- axi_traffic_gen/data/axi_traffic_gen.tcl
+++ axi_traffic_gen/data/axi_traffic_gen.tcl
@@ -1,3 +1,30 @@
 proc generate {drv_handle} {
+	# try to source the common tcl procs
+	# assuming the order of return is based on repo priority
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+	# set up interrupt-names
+	set intr_list "irq_out err_out"
+	set interrupts ""
+	set interrupt_names ""
+	foreach irq ${intr_list} {
+		set intr_info [get_intr_id $drv_handle $irq]
+		if { [string match -nocase $intr_info "-1"] } {
+			error "ERROR:${drv_handle}: $irq port is not connected"
+		}
+		if { [string match -nocase $interrupt_names ""] } {
+			set interrupt_names "$irq"
+			set interrupts "$intr_info"
+		} else {
+			append interrupt_names " " "$irq"
+			append interrupts " " "$intr_info"
+		}
+	}
+	hsm::utils::add_new_property $drv_handle "interrupts" int $interrupts
+	hsm::utils::add_new_property $drv_handle "interrupt-names" stringlist $interrupt_names
 }
-
