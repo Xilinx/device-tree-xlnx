8bb20210ed3e7890ad0b1ec693bfc7b6700bc553
Michal Simek <michal.simek@xilinx.com>
2014-08-11 09:55:18 +0200

    Generate proper Zynq CANPS clock names
    This binding is compatible with v3.16 mailine
    Linux kernel.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Jason Wu <j.wu@xilinx.com>

--- canps/data/canps.tcl
+++ canps/data/canps.tcl
@@ -1,2 +1,8 @@
 proc generate {drv_handle} {
+	set kernel_version [get_property CONFIG.kernel_version [get_os]]
+	switch -exact $kernel_version {
+		"2014.3" {
+			hsm::utils::add_new_property $drv_handle "clock-names" stringlist "can_clk pclk"
+		}
+	}
 }
