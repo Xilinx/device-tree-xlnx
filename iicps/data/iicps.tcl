29182fadf9e4bb6d22514018dd74404958355568
Michal Simek <michal.simek@xilinx.com>
2014-08-28 15:24:53 +0200

    iicps: Do not generate bus-id
    It is completely unused by the kernel.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Jason Wu <j.wu@xilinx.com>

--- iicps/data/iicps.tcl
+++ iicps/data/iicps.tcl
@@ -12,14 +12,5 @@ proc ps7_reset_handle {drv_handle reset_pram conf_prop} {
 }
 
 proc generate {drv_handle} {
-    set i2c_count [hsm::utils::get_os_parameter_value "i2c_count"]
-    if { [llength $i2c_count] == 0 } {
-        set i2c_count 0
-    }
-    set_property CONFIG.bus-id "$i2c_count" $drv_handle
-    incr i2c_count
-    hsm::utils::set_os_parameter_value "i2c_count" $i2c_count
-    set i2c_count [hsm::utils::get_os_parameter_value "i2c_count"]
-
     ps7_reset_handle $drv_handle C_I2C_RESET i2c-reset
 }
