940efb3d992f51848d6f3962ab2a3cc223d76eda
Jason Wu <j.wu@xilinx.com>
2014-09-23 19:16:53 +1000

    uartps: Initial adaption of new APIs
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- uartps/data/uartps.tcl
+++ uartps/data/uartps.tcl
@@ -1,9 +1,15 @@
 proc generate {drv_handle} {
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
+    }
     set ip [get_cells $drv_handle]
-
-    set consoleip [get_property CONFIG.console_device [get_os] ]
+    set consoleip [get_property CONFIG.console_device [get_os]]
     set port_number 0
-    if { [string match -nocase "$ip" "$consoleip"]  == 0 } {
+    if {[string match -nocase "$ip" "$consoleip"] == 0} {
         set serial_count [hsm::utils::get_os_parameter_value "serial_count"]
         if { [llength $serial_count]  == 0 } {
             set serial_count 0
