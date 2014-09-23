33e020a7aee375e02fd217997d2a17afaf248b89
Jason Wu <j.wu@xilinx.com>
2014-09-23 18:18:34 +1000

    ddrps: Initial adaption of new APIs
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- ddrps/data/ddrps.tcl
+++ ddrps/data/ddrps.tcl
@@ -1,21 +1,35 @@
-proc get_baseaddr { slave_ip } {
-    set mem_range [lindex [xget_ip_mem_ranges $slave_ip] 0]
-    return [get_property BASE_VALUE $mem_range]
-}
-proc get_highaddr { slave_ip } {
-    set mem_range [lindex [xget_ip_mem_ranges $slave_ip] 0]
-    return [get_property HIGH_VALUE $mem_range]
+# workaround for ps7 ddrc has none zero start address
+proc gen_ps7_ddr_reg_property {drv_handle} {
+    proc_called_by
+    set reg ""
+    set slave [get_cells ${drv_handle}]
+    set ip_mem_handles [hsi::utils::get_ip_mem_ranges $slave]
+    foreach mem_handle ${ip_mem_handles} {
+        #set base [get_property BASE_VALUE $mem_handle]
+        set base 0x0
+        set high [get_property HIGH_VALUE $mem_handle]
+        set size [format 0x%x [expr {${high} - ${base} + 1}]]
+        if {[string_is_empty $reg]} {
+            set reg "$base $size"
+        } else {
+            # ensure no duplication
+            if {![regexp ".*${reg}.*" "$base $size" matched]} {
+                set reg "$reg $base $size"
+            }
+        }
+    }
+    set_drv_prop_if_empty $drv_handle reg $reg intlist
 }
 
+
 proc generate {drv_handle} {
-    set ip [get_cells $drv_handle]
-    set baseaddr [get_baseaddr $ip]
-    set highaddr [get_highaddr $ip]
-    set main_memory [get_property CONFIG.main_memory [get_os]]
-    if { [string match -nocase "$main_memory" $ip] }  {
-        set baseaddr "0x0"
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
     }
-    set size [expr $highaddr -$baseaddr + 1]
-    set value [format "0x%x 0x%x" $baseaddr $size]
-    set_property CONFIG.reg "$value" $drv_handle
+    gen_ps7_ddr_reg_property $drv_handle
+    add_memory_node $drv_handle
 }
