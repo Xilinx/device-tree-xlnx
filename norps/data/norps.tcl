c4e877d36e836c3f137e91dc61147b94db52fef5
Jason Wu <j.wu@xilinx.com>
2014-09-29 21:18:53 +1000

    norps: Enable reg generation
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- norps/data/norps.tcl
+++ norps/data/norps.tcl
@@ -1,3 +1,13 @@
 proc generate {drv_handle} {
-    # if addr25 is used, should we consider set the reg size to 64MB?
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
+    }
+
+    # TODO: if addr25 is used, should we consider set the reg size to 64MB?
+    # enable reg generation for ps ip
+    gen_reg_property $drv_handle "enable_ps_ip"
 }
