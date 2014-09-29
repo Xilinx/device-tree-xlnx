dec341132417a3c191571e12a967265c36ae1955
Jason Wu <j.wu@xilinx.com>
2014-09-29 15:07:56 +1000

    intc: Initial 2014.4 HSM port
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- intc/data/intc.tcl
+++ intc/data/intc.tcl
@@ -1,7 +1,15 @@
 proc generate {drv_handle} {
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
+    }
+
     set ip [get_cells $drv_handle]
-    set num_intr_inputs [get_ip_param_value $ip C_NUM_INTR_INPUTS]
-    set kind_of_intr    [get_ip_param_value $ip C_KIND_OF_INTR]
+    set num_intr_inputs [hsi::utils::get_ip_param_value $ip C_NUM_INTR_INPUTS]
+    set kind_of_intr [hsi::utils::get_ip_param_value $ip C_KIND_OF_INTR]
     # Pad to 32 bits - num_intr_inputs
     if { $num_intr_inputs != -1 } {
         set count 0
@@ -18,4 +26,5 @@ proc generate {drv_handle} {
         set kind_of_intr 0
     }
     set_property CONFIG.xlnx,kind-of-intr $kind_of_intr $drv_handle
+    set_drv_conf_prop $drv_handle C_NUM_INTR_INPUTS "xlnx,num-intr-inputs"
 }
