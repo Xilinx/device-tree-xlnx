2125ed2236fbe4b2177611bc3325e48e41d28ed9
Jason Wu <j.wu@xilinx.com>
2014-09-29 15:08:32 +1000

    mig_7series: Initial 2014.4 HSI port
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- mig_7series/data/mig_7series.tcl
+++ mig_7series/data/mig_7series.tcl
@@ -1,2 +1,10 @@
 proc generate {drv_handle} {
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
+    }
+    add_memory_node $drv_handle
 }
