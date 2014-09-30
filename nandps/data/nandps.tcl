78fd936c8f2ab7ce98e0fa74c92a2ac5d06d25d6
Jason Wu <j.wu@xilinx.com>
2014-09-30 15:01:11 +1000

    nandps: Fix IP parameter name change between 2014.4 and 2014.2
    In 2014.2 the IP parameter name is C_NAND_CYCLE_T0, this has changed in
    2014.4 to NAND-CYCLE-T0.
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- nandps/data/nandps.tcl
+++ nandps/data/nandps.tcl
@@ -9,11 +9,23 @@ proc generate {drv_handle} {
         }
     }
 
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T0" "arm,nand-cycle-t0"
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T1" "arm,nand-cycle-t1"
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T2" "arm,nand-cycle-t2"
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T3" "arm,nand-cycle-t3"
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T4" "arm,nand-cycle-t4"
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T5" "arm,nand-cycle-t5"
-    set_drv_conf_prop $drv_handle "C_NAND_CYCLE_T6" "arm,nand-cycle-t6"
+    set hw_ver [get_hw_version]
+    # Parameter name changed in 2014.4
+    # TODO: check with 2014.3
+    switch -exact $hw_ver {
+        "2014.2" {
+             set nand_par_prefix "C_NAND_CYCLE_"
+        } "2014.4" -
+        default {
+            set nand_par_prefix "NAND-CYCLE-"
+        }
+    }
+
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T0" "arm,nand-cycle-t0"
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T1" "arm,nand-cycle-t1"
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T2" "arm,nand-cycle-t2"
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T3" "arm,nand-cycle-t3"
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T4" "arm,nand-cycle-t4"
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T5" "arm,nand-cycle-t5"
+    set_drv_conf_prop $drv_handle "${nand_par_prefix}T6" "arm,nand-cycle-t6"
 }
