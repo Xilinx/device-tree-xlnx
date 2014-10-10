66c16ddce6c831f9b769a47524b7b61a0cc74e47
Jason Wu <j.wu@xilinx.com>
2014-10-10 11:58:28 +1000

    axi_can: Update to match 2014.4 kenerl driver
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- axi_can/data/axi_can.tcl
+++ axi_can/data/axi_can.tcl
@@ -10,4 +10,15 @@ proc generate {drv_handle} {
     }
     set_drv_conf_prop $drv_handle c_can_tx_dpth xlnx,can-tx-dpth hexint
     set_drv_conf_prop $drv_handle c_can_rx_dpth xlnx,can-rx-dpth hexint
+
+    set proc_type [get_sw_proc_prop IP_NAME]
+    switch $proc_type {
+        "ps7_cortexa9" {
+            # for 2014.4
+            set_drv_prop_if_empty $drv_handle "clock-names" "s_axi_aclk" stringlist
+        } "microblaze" {}
+        default {
+            error "Unknown arch"
+        }
+    }
 }
