1e0decfc03cc07c1b34e2837a89997c5834b8100
Jason Wu <j.wu@xilinx.com>
2014-10-21 13:34:23 +1000

    nandps: Convert time unit to clock cycles
    This is required in 2014.4 as it is no longer export the value in clock
    cycle but instead as nano seconds.
    
    Signed-off-by: Jason Wu <j.wu@xilinx.com>

--- nandps/data/nandps.tcl
+++ nandps/data/nandps.tcl
@@ -1,3 +1,7 @@
+proc ns_to_cycle {drv_handle prop_name nand_cycle_time} {
+    return [expr [get_property CONFIG.$prop_name [get_cells $drv_handle]]/${nand_cycle_time} + 1]
+}
+
 proc generate {drv_handle} {
     # try to source the common tcl procs
     # assuming the order of return is based on repo priority
@@ -15,17 +19,20 @@ proc generate {drv_handle} {
     switch -exact $hw_ver {
         "2014.2" {
              set nand_par_prefix "C_NAND_CYCLE_"
+             set nand_clk_freq 1
         } "2014.4" -
         default {
             set nand_par_prefix "NAND-CYCLE-"
+            set nand_cycle_time [expr "1000000000/[get_property CONFIG.C_NAND_CLK_FREQ_HZ [get_cells $drv_handle]]"]
         }
     }
 
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T0" "arm,nand-cycle-t0"
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T1" "arm,nand-cycle-t1"
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T2" "arm,nand-cycle-t2"
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T3" "arm,nand-cycle-t3"
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T4" "arm,nand-cycle-t4"
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T5" "arm,nand-cycle-t5"
-    set_drv_conf_prop $drv_handle "${nand_par_prefix}T6" "arm,nand-cycle-t6"
+    set_drv_prop $drv_handle "arm,nand-cycle-t0" [ns_to_cycle $drv_handle "${nand_par_prefix}T0" $nand_cycle_time]
+    set_drv_prop $drv_handle "arm,nand-cycle-t1" [ns_to_cycle $drv_handle "${nand_par_prefix}T1" $nand_cycle_time]
+    set_drv_prop $drv_handle "arm,nand-cycle-t2" [ns_to_cycle $drv_handle "${nand_par_prefix}T2" $nand_cycle_time]
+    set_drv_prop $drv_handle "arm,nand-cycle-t3" [ns_to_cycle $drv_handle "${nand_par_prefix}T3" $nand_cycle_time]
+    set_drv_prop $drv_handle "arm,nand-cycle-t4" [ns_to_cycle $drv_handle "${nand_par_prefix}T4" $nand_cycle_time]
+    set_drv_prop $drv_handle "arm,nand-cycle-t5" [ns_to_cycle $drv_handle "${nand_par_prefix}T5" $nand_cycle_time]
+    set_drv_prop $drv_handle "arm,nand-cycle-t6" [ns_to_cycle $drv_handle "${nand_par_prefix}T6" $nand_cycle_time]
+
 }
