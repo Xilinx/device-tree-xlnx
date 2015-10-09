7e83682a5269f626238d3ca6df8a0ba515ce1a30
Naga Sureshkumar Relli <nagasure@xilinx.com>
2015-10-09 11:33:40 +0530

    Add separate driver for cortex53
    This patch adds new driver cpu_cortexa53 to
    support ZynqMP.
    
    Signed-off-by: Naga Sureshkumar Relli <nagasure@xilinx.com>

---
+++ cpu_cortexa53/data/cpu_cortexa53.tcl
@@ -0,0 +1,30 @@
+#
+# (C) Copyright 2014-2015 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	global dtsi_fname
+	set dtsi_fname "zynqmp/zynqmp.dtsi"
+
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+
+	# create root node
+	set master_root_node [gen_root_node $drv_handle]
+	set nodes [gen_cpu_nodes $drv_handle]
+}
