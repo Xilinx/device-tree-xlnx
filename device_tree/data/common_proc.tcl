71dcd0fce64e48eb44a131ef0dde96e01e14c2df
Naga Sureshkumar Relli <nagasure@xilinx.com>
2015-10-20 17:44:13 +0530

    Add clock info for ZynqMP
    Add zynqmp-clk.dtsi for clock definitions to ZynqMP.
    
    Signed-off-by: Naga Sureshkumar Relli <nagasure@xilinx.com>
    Acked-by: Michal Simek <michal.simek@xilinx.com>

--- device_tree/data/common_proc.tcl
+++ device_tree/data/common_proc.tcl
@@ -1651,6 +1651,7 @@ proc gen_root_node {drv_handle} {
 			create_dt_tree_from_dts_file
 			global dtsi_fname
 			update_system_dts_include [file tail ${dtsi_fname}]
+			update_system_dts_include [file tail "zynqmp-clk.dtsi"]
 			# no root_node required as zynqmp.dtsi
 			return 0
 		}
