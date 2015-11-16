d3a1a42da28c23ea3d95ce3d8d244a00f03adfa1
Naga Sureshkumar Relli <nagasure@xilinx.com>
2015-11-16 14:39:33 +0530

    Update serial alias reference
    The generated serial alias(serial0) is not refering to selected
    console device. this patch updates this such that serial0 will
    always refer to console device
    
    Signed-off-by: Naga Sureshkumar Relli <nagasure@xilinx.com>
    Acked-by: Michal Simek <michal.simek@xilinx.com>

--- device_tree/data/device_tree.tcl
+++ device_tree/data/device_tree.tcl
@@ -214,6 +214,28 @@ proc update_alias {os_handle} {
 		set all_drivers [lreplace $all_drivers 0 0 $qspi_element]
 		set all_drivers [lreplace $all_drivers $pos $pos $first_element]
     }
+	# Update all_drivers list such that console device should be the first
+	# uart device in the list.
+	set console_ip [get_property CONFIG.console_device [get_os]]
+	foreach drv_handle $all_drivers {
+		set alias_str [get_property CONFIG.dtg.alias $drv_handle]
+		if {[string match -nocase $alias_str "serial"]} {
+			if {[string match $console_ip $drv_handle] == 0} {
+				# break the loop After swaping console device and uart device
+				# found in list
+				set consoleip_pos [lsearch $all_drivers $console_ip]
+				set first_occur_pos [lsearch $all_drivers $drv_handle]
+				set console_element [lindex $all_drivers $consoleip_pos]
+				set uart_element [lindex $all_drivers $first_occur_pos]
+				set all_drivers [lreplace $all_drivers $consoleip_pos $consoleip_pos $uart_element]
+				set all_drivers [lreplace $all_drivers $first_occur_pos $first_occur_pos $console_element]
+				break
+			} else {
+				# if the first uart device in the list is console device
+				break
+			}
+		}
+	}
 
 	foreach drv_handle $all_drivers {
         set tmp [list_property $drv_handle CONFIG.dtg.alias]
