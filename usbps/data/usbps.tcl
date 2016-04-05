572bd9c54d277d62ef3db1c2bd0d1971cfdf50d1
Naga Sureshkumar Relli <nagasure@xilinx.com>
2016-04-05 16:54:49 +0530

    Fix race condition while generating usb node
    This patch generates usb status okay property based on psu_usb ip
    enabled,i.e if psu_usb_0 enabled then will generate usb0 ref node
    in dts and also generate phy_type property only for ps7_usb.
    
    Signed-off-by: Naga Sureshkumar Relli <nagasure@xilinx.com>
    Acked-by: Michal Simek <michal.simek@xilinx.com>

--- usbps/data/usbps.tcl
+++ usbps/data/usbps.tcl
@@ -22,4 +22,12 @@ proc generate {drv_handle} {
     }
 
     ps7_reset_handle $drv_handle CONFIG.C_USB_RESET CONFIG.usb-reset
+    set proctype [get_property IP_NAME [get_cells -hier [get_sw_processor]]]
+    if {[string match -nocase $proctype "ps7_cortexa9"] } {
+        set_drv_prop $drv_handle phy_type ulpi string
+    } else {
+        set index [string index $drv_handle end]
+        set rt_node [add_or_get_dt_node -n usb -l usb$index -d "system.dts" -auto_ref_parent]
+        hsi::utils::add_new_dts_param "${rt_node}" "status" "okay" string
+    }
 }
