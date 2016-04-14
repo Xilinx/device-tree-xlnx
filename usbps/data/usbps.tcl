1ee63b0270d4285b021977bbdfbfbf9509d38591
Naga Sureshkumar Relli <nagasure@xilinx.com>
2016-04-14 12:41:23 +0530

    Don't hardcode dts file name
    Instead of using hardcoded dts file, read it form default
    dts config property.
    
    Signed-off-by: Naga Sureshkumar Relli <nagasure@xilinx.com>
    Reviewed-by: Kedareswara rao Appana <appanad@xilinx.com>

--- usbps/data/usbps.tcl
+++ usbps/data/usbps.tcl
@@ -23,11 +23,12 @@ proc generate {drv_handle} {
 
     ps7_reset_handle $drv_handle CONFIG.C_USB_RESET CONFIG.usb-reset
     set proctype [get_property IP_NAME [get_cells -hier [get_sw_processor]]]
+    set default_dts [set_drv_def_dts $drv_handle]
     if {[string match -nocase $proctype "ps7_cortexa9"] } {
         set_drv_prop $drv_handle phy_type ulpi string
     } else {
         set index [string index $drv_handle end]
-        set rt_node [add_or_get_dt_node -n usb -l usb$index -d "system.dts" -auto_ref_parent]
+        set rt_node [add_or_get_dt_node -n usb -l usb$index -d $default_dts -auto_ref_parent]
         hsi::utils::add_new_dts_param "${rt_node}" "status" "okay" string
     }
 }
