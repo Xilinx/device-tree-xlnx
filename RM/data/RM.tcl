8f22d0332a54604d2ca8853abbdeafb5c07c4ca0
Michal Simek <michal.simek@xilinx.com>
2017-02-23 13:35:48 +0100

    Add RM support as fpga_region
    Generate at least RM fragment and for zynq link to devcfg.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Naga Sureshkumar Relli <nagasure@xilinx.com>

---
+++ RM/data/RM.tcl
@@ -0,0 +1,22 @@
+#
+# (C) Copyright 2017 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	set val [get_property FAMILY [get_hw_designs]]
+	switch -glob $val {
+		"zynq" {
+			hsi::utils::add_new_property $drv_handle "fpga-mgr" string "<&devcfg>"
+		}
+	}
+}
