dd6d31947838700cb656b7c274a8dd4f077f2f88
Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
2017-06-27 14:33:08 +0530

    rfdc: Add support for Caher AMS
    This patch adds usp_rf_data_converter support.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmis@xilinx.com>
    Acked-by: Michal Simek <michals@xilinx.com>

---
+++ rfdc/data/rfdc.tcl
@@ -0,0 +1,25 @@
+#
+# (C) Copyright 2017 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	# try to source the common tcl procs
+	# assuming the order of return is based on repo priority
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+}
