bcbcfb812a24a0ecb319495a1cb5d9a9dfd0da86
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2017-08-11 13:43:09 +0530

    Add the bus-width parameters in the qspi node
    Update these spi-tx-bus-width and spi-rx-bus-width
    parameters based on the bus_width.
    
    CR:982026
    Signed-off-by: Venkatesh Yadav Abbarapu <vabbarap@xilinx.com>
    Acked-by: Michal Simek <michals@xilinx.com>

--- qspips/data/qspips.tcl
+++ qspips/data/qspips.tcl
@@ -13,15 +13,45 @@
 #
 
 proc generate {drv_handle} {
-    set slave [get_cells -hier $drv_handle]
-    set qspi_mode [hsi::utils::get_ip_param_value $slave "C_QSPI_MODE"]
-    if { $qspi_mode == 2} {
-        set is_dual 1
-    } else {
-        set is_dual 0
-    }
-    set_property CONFIG.is-dual $is_dual $drv_handle
+	foreach i [get_sw_cores device_tree] {
+	set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+	if {[file exists $common_tcl_file]} {
+		source $common_tcl_file
+		break
+		}
+	}
 
+	set slave [get_cells -hier $drv_handle]
+	set qspi_mode [hsi::utils::get_ip_param_value $slave "C_QSPI_MODE"]
+	if { $qspi_mode == 2} {
+		set is_dual 1
+	} else {
+		set is_dual 0
+	}
+	set_property CONFIG.is-dual $is_dual $drv_handle
+	set bus_width [get_property CONFIG.C_QSPI_BUS_WIDTH [get_cells -hier $drv_handle]]
+
+	switch $bus_width {
+		"3" {
+			hsi::utils::add_new_property $drv_handle "spi-tx-bus-width" int 8
+			hsi::utils::add_new_property $drv_handle "spi-rx-bus-width" int 8
+		}
+		"2" {
+			hsi::utils::add_new_property $drv_handle "spi-tx-bus-width" int 4
+			hsi::utils::add_new_property $drv_handle "spi-rx-bus-width" int 4
+		}
+		"1" {
+			hsi::utils::add_new_property $drv_handle "spi-tx-bus-width" int 2
+			hsi::utils::add_new_property $drv_handle "spi-rx-bus-width" int 2
+		}
+		"0" {
+			hsi::utils::add_new_property $drv_handle "spi-tx-bus-width" int 1
+			hsi::utils::add_new_property $drv_handle "spi-rx-bus-width" int 1
+		}
+		default {
+			dtg_warning "Unsupported bus_width:$bus_width"
+		}
+	}
     # these are board level information
     # set primary_flash [hsi::utils::add_new_child_node $drv_handle "primary_flash"]
     # hsi::utils::add_new_property $primary_flash "dts.device_type" string "ps7-qspi"
