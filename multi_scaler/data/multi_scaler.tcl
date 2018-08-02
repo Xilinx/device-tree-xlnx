3e85f4a97072e4293dfb06df59c4ef7d20ccf74c
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2018-08-02 04:21:22 -0400

    multi_scaler: Add video multi scaler IP support
    XM2MSC is a Scaler which consists of maximum 8 memory to
    memory path/Channel, which can perform scaling and color space conversion.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

---
+++ multi_scaler/data/multi_scaler.tcl
@@ -0,0 +1,135 @@
+#
+# (C) Copyright 2018 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+	set node [gen_peripheral_nodes $drv_handle]
+	if {$node == 0} {
+		return
+	}
+	set compatible [get_comp_str $drv_handle]
+	set compatible [append compatible " " "xlnx,v-multi-scaler-v1.0"]
+	set_drv_prop $drv_handle compatible "$compatible" stringlist
+	set ip [get_cells -hier $drv_handle]
+	set max_outs [get_property CONFIG.MAX_OUTS [get_cells -hier $drv_handle]]
+	hsi::utils::add_new_dts_param "$node" "xlnx,max-chan" $max_outs int
+	set max_cols [get_property CONFIG.MAX_COLS [get_cells -hier $drv_handle]]
+	hsi::utils::add_new_dts_param "$node" "xlnx,max-width" $max_cols int
+	set max_rows [get_property CONFIG.MAX_ROWS [get_cells -hier $drv_handle]]
+	hsi::utils::add_new_dts_param "$node" "xlnx,max-height" $max_rows int
+	set taps [get_property CONFIG.TAPS [get_cells -hier $drv_handle]]
+	hsi::utils::add_new_dts_param "$node" "xlnx,num-taps" $taps int
+	set aximm_addr_width [get_property CONFIG.AXIMM_ADDR_WIDTH [get_cells -hier $drv_handle]]
+	hsi::utils::add_new_dts_param "$node" "xlnx,dma-addr-width" $aximm_addr_width hexint
+	set has_bgr8 [get_property CONFIG.HAS_BGR8 [get_cells -hier $drv_handle]]
+	set vid_formats ""
+	if {$has_bgr8 == 1} {
+		append vid_formats " " "rgb888"
+	}
+	set has_bgra8 [get_property CONFIG.HAS_BGRA8 [get_cells -hier $drv_handle]]
+	if {$has_bgra8 == 1} {
+		append vid_formats " " "argb8888"
+	}
+	set has_bgrx8 [get_property CONFIG.HAS_BGRX8 [get_cells -hier $drv_handle]]
+	if {$has_bgrx8 == 1} {
+		append vid_formats " " "xbgr8888"
+	}
+	set has_rgb8 [get_property CONFIG.HAS_RGB8 [get_cells -hier $drv_handle]]
+	if {$has_rgb8 == 1} {
+		append vid_formats " " "bgr888"
+	}
+	set has_rgba8 [get_property CONFIG.HAS_RGBA8 [get_cells -hier $drv_handle]]
+	if {$has_rgba8 == 1} {
+		append vid_formats " " "abgr8888"
+	}
+	set has_rgbx10 [get_property CONFIG.HAS_RGBX10 [get_cells -hier $drv_handle]]
+	if {$has_rgbx10 == 1} {
+		append vid_formats " " "xbgr2101010"
+	}
+	set has_uyuy8 [get_property CONFIG.HAS_UYVY8 [get_cells -hier $drv_handle]]
+	if {$has_uyuy8 == 1} {
+		append vid_formats " " "uyuy"
+	}
+	set has_y8 [get_property CONFIG.HAS_Y8 [get_cells -hier $drv_handle]]
+	if {$has_y8 == 1} {
+		append vid_formats " " "y8"
+	}
+	set has_y10 [get_property CONFIG.HAS_Y10 [get_cells -hier $drv_handle]]
+	if {$has_y10 == 1} {
+		append vid_formats " " "y10"
+	}
+	set has_yuv8 [get_property CONFIG.HAS_YUV8 [get_cells -hier $drv_handle]]
+	if {$has_yuv8 == 1} {
+		append vid_formats " " "vuy888"
+	}
+	set has_yuvx8 [get_property CONFIG.HAS_YUVX8 [get_cells -hier $drv_handle]]
+	if {$has_yuvx8 == 1} {
+		append vid_formats " " "xvuy8888"
+	}
+	set has_yuvx10 [get_property CONFIG.HAS_YUVX10 [get_cells -hier $drv_handle]]
+	if {$has_yuvx10 == 1} {
+		append vid_formats " " "yuvx2101010"
+	}
+	set has_yuyv8 [get_property CONFIG.HAS_YUYV8 [get_cells -hier $drv_handle]]
+	if {$has_yuyv8 == 1} {
+		append vid_formats " " "yuyv"
+	}
+	set has_y_uv8_420 [get_property CONFIG.HAS_Y_UV8_420 [get_cells -hier $drv_handle]]
+	if {$has_y_uv8_420 == 1} {
+		append vid_formats " " "nv12"
+	}
+	set has_y_uv8 [get_property CONFIG.HAS_Y_UV8 [get_cells -hier $drv_handle]]
+	if {$has_y_uv8 == 1} {
+		append vid_formats " " "nv16"
+	}
+	set has_y_uv10 [get_property CONFIG.HAS_Y_UV10 [get_cells -hier $drv_handle]]
+	if {$has_y_uv10 == 1} {
+		append vid_formats " " "xv20"
+	}
+	set has_y_uv10_420 [get_property CONFIG.HAS_Y_UV10_420 [get_cells -hier $drv_handle]]
+	if {$has_y_uv10_420 == 1} {
+		append vid_formats " " "xv15"
+	}
+	hsi::utils::add_new_dts_param "${node}" "xlnx,vid-formats" $vid_formats stringlist
+	set connected_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "ap_rst_n"]
+	set pins [get_pins -of_objects [get_nets -of_objects [get_pins -of_objects $ip "ap_rst_n"]]]
+	foreach pin $pins {
+		set sink_periph [::hsi::get_cells -of_objects $pin]
+		set sink_ip [get_property IP_NAME $sink_periph]
+		if {[string match -nocase $sink_ip "xlslice"]} {
+			set gpio [get_property CONFIG.DIN_FROM $sink_periph]
+			set pins [get_pins -of_objects [get_nets -of_objects [get_pins -of_objects $sink_periph "Din"]]]
+			foreach pin $pins {
+					set periph [::hsi::get_cells -of_objects $pin]
+					set ip [get_property IP_NAME $periph]
+					set proc_type [get_sw_proc_prop IP_NAME]
+					if {[string match -nocase $proc_type "psu_cortexa53"] } {
+						if {[string match -nocase $ip "zynq_ultra_ps_e"]} {
+							set gpio [expr $gpio + 78]
+							hsi::utils::add_new_dts_param "$node" "reset-gpios" "gpio $gpio 1" reference
+						}
+					}
+					if {[string match -nocase $ip "axi_gpio"]} {
+						hsi::utils::add_new_dts_param "$node" "reset-gpios" "$periph $gpio 0 1" reference
+					}
+			}
+		}
+	}
+}
