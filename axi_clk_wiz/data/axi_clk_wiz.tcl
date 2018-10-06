acf0fc7bfa78dc265671f26a7c2bb4081dec0669
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2018-10-06 14:02:29 +0530

    axi_clk_wiz: Fix the speed-grade property
    As per the IP documentation the speed grade values are -1, -2, -3,
    -1Q, -1HE, and so on. If the speed grade contains the alpha-numeric
    values, retrive the number and update in the DT.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Reviewed-by: Shubhrajyoti Datta <shubhraj@xilinx.com>

--- axi_clk_wiz/data/axi_clk_wiz.tcl
+++ axi_clk_wiz/data/axi_clk_wiz.tcl
@@ -26,14 +26,7 @@ proc generate {drv_handle} {
 	set compatible [append compatible " " "xlnx,clocking-wizard"]
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
 	set ip [get_cells -hier $drv_handle]
-	set proctype [get_property IP_NAME [get_cells -hier [get_sw_processor]]]
-	if {[string match -nocase $proctype "ps7_cortexa9"] } {
-		set speedgrade "([get_property SPEEDGRADE [get_hw_designs]])"
-		if {![string equal $speedgrade "()"]} {
-			hsi::utils::add_new_property $drv_handle "speed-grade" int $speedgrade
-		}
-	}
-
+	gen_speedgrade $drv_handle
 	set output_names "clk_out1 clk_out2 clk_out3 clk_out4 clk_out5 clk_out6 clk_out7"
 	set_property CONFIG.clock-output-names $output_names $drv_handle
 
@@ -43,5 +36,13 @@ proc generate {drv_handle} {
 	set proctype [get_property IP_NAME $proc_ip]
 	if {[string match -nocase $proctype "microblaze"] } {
 		gen_dev_ccf_binding $drv_handle "clk_in1 s_axi_aclk" "clocks clock-names"
+	}
+}
+
+proc gen_speedgrade {drv_handle} {
+	set speedgrade [get_property SPEEDGRADE [get_hw_designs]]
+	set num [regexp -all -inline -- {[0-9]} $speedgrade]
+	if {![string equal $num ""]} {
+		hsi::utils::add_new_property $drv_handle "speed-grade" int $num
 	}
 }
