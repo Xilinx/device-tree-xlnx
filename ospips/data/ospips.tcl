2c138b5e92f42bfc155e5ce3e0e19403c6d7a68d
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2019-01-25 09:28:58 +0530

    ospi: Add PS OSPI IP support
    Adding the ospi support in the DTG.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

---
+++ ospips/data/ospips.tcl
@@ -0,0 +1,24 @@
+#
+# (C) Copyright 2019 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	foreach i [get_sw_cores device_tree] {
+	set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+	if {[file exists $common_tcl_file]} {
+		source $common_tcl_file
+		break
+		}
+	}
+
+}
