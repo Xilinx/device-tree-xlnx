15e325d081d49ee8c4cd25e082c3b81242b6f87e
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2019-01-28 08:59:35 +0530

    emacps: Fix the clock frequency param in tsu_ext_clk node
    In the tsu_ext_clk node the clock frequency param should be
    clock-frequency rather than tsu-clk-freq, fixing this.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Reviewed-by: Harini Katakam <harini.katakam@xilinx.com>

--- emacps/data/emacps.tcl
+++ emacps/data/emacps.tcl
@@ -120,7 +120,7 @@ proc generate {drv_handle} {
                 hsi::utils::add_new_dts_param "${tsu_node}" "compatible" "fixed-clock" stringlist
                 hsi::utils::add_new_dts_param "${tsu_node}" "#clock-cells" 0 int
                 set tsu-clk-freq [get_property CONFIG.C_ENET_TSU_CLK_FREQ_HZ [get_cells -hier $drv_handle]]
-                hsi::utils::add_new_dts_param "${tsu_node}" "tsu-clk-freq" ${tsu-clk-freq} int
+                hsi::utils::add_new_dts_param "${tsu_node}" "clock-frequency" ${tsu-clk-freq} int
                 set_drv_prop_if_empty $drv_handle "clock-names" "pclk hclk tx_clk rx_clk tsu_clk" stringlist
                 set_drv_prop_if_empty $drv_handle "clocks" "clk 31>, <&clk 107>, <&clk 48>, <&clk 52>, <&tsu_ext_clk" reference
             }
