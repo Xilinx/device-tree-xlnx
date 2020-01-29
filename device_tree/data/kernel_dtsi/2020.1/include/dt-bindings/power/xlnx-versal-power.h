62be36e3c1a439795f64c23e59f41bbf595ff664
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2020-01-29 01:53:30 -0700

    kernel:v2020.1:Sync the versal clk dts from U-boot
    The U-boot changes are
    1)dts: versal: Correct power domain ID for PMC GPIO.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

--- device_tree/data/kernel_dtsi/2020.1/include/dt-bindings/power/xlnx-versal-power.h
+++ device_tree/data/kernel_dtsi/2020.1/include/dt-bindings/power/xlnx-versal-power.h
@@ -25,6 +25,7 @@
 #define PM_DEV_SWDT_FPD				(0x18224029U)
 #define PM_DEV_OSPI				(0x1822402aU)
 #define PM_DEV_QSPI				(0x1822402bU)
+#define PM_DEV_GPIO_PMC				(0x1822402cU)
 #define PM_DEV_SDIO_0				(0x1822402eU)
 #define PM_DEV_SDIO_1				(0x1822402fU)
 #define PM_DEV_RTC				(0x18224034U)
