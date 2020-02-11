fca938bf9ddf7313fa1d89ef5f3aeb1342708e97
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2020-02-10 20:42:42 -0700

    debug_bridge: Add initial support for debug bridge IP
    Adding the initial support for debug bridge IP.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

---
+++ debug_bridge/data/debug_bridge.tcl
@@ -0,0 +1,32 @@
+#
+# (C) Copyright 2020 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	# try to source the common tcl procs
+	# assuming the order of return is based on repo priority
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+	set node [gen_peripheral_nodes $drv_handle]
+	if {$node == 0} {
+		return
+	}
+	set compatible [get_comp_str $drv_handle]
+	set compatible [append compatible " " "generic-uio"]
+	set_drv_prop $drv_handle compatible "$compatible" stringlist
+}
