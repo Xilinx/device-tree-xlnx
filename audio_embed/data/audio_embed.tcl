a88c81da8322e531a5ee4f65aee8e5476e0b7106
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2020-02-29 03:29:54 -0700

    Removing hardcoded video pipelines for multimedia IPs
    From the hdf/xsa there is no sufficient data to create video pipelines
    nodes, so from DTG we created the video pipeline nodes by hardcoding the
    endpoints and remote endpoints. This is breaking when someone designs
    has different combinations of video pipelines rather than the supported
    ones in the DTG. Till we get the sufficient endpoint/remote endpoint
    information in the hdf/xsa, we are removing the generation of video
    pipelines.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

--- audio_embed/data/audio_embed.tcl
+++ audio_embed/data/audio_embed.tcl
@@ -27,34 +27,4 @@ proc generate {drv_handle} {
 	set compatible [get_comp_str $drv_handle]
 	set compatible [append compatible " " "xlnx,v-uhdsdi-audio-2.0"]
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
-	set connected_embed_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "SDI_EMBED_ANC_DS_IN"]
-	if {[llength $connected_embed_ip] != 0} {
-		set connected_embed_ip_type [get_property IP_NAME $connected_embed_ip]
-		if {[string match -nocase $connected_embed_ip_type "v_smpte_uhdsdi_tx_ss"]} {
-			set sdi_av_port [add_or_get_dt_node -n "port" -l sdi_av_port -u 0 -p $node]
-			hsi::utils::add_new_dts_param "$sdi_av_port" "reg" 0 int
-			set sdi_embed_node [add_or_get_dt_node -n "endpoint" -l sditx_audio_embed_src -p $sdi_av_port]
-			hsi::utils::add_new_dts_param "$sdi_embed_node" "remote-endpoint" sdi_audio_sink_port reference
-		}
-	} else {
-		dtg_warning "$drv_handle connected_ip is NULL for the pin SDI_EMBED_ANC_DS_IN"
-	}
-	set connected_extract_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "SDI_EXTRACT_ANC_DS_IN"]
-	if {[llength $connected_extract_ip] != 0} {
-		hsi::utils::add_new_dts_param "$node" "xlnx,sdi-rx-video" $connected_extract_ip reference
-	} else {
-		dtg_warning "$drv_handle connected_extract_ip is NULL for the pin SDI_EXTRACT_ANC_DS_IN"
-	}
-	set connected_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "S_AXIS_DATA"]
-	if {[llength $connected_ip] != 0} {
-		hsi::utils::add_new_dts_param "$node" "xlnx,snd-pcm" $connected_ip reference
-	} else {
-		dtg_warning "$drv_handle connected ip is NULL for the pin S_AXIS_DATA"
-	}
-	set connect_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "M_AXIS_DATA"]
-	if {[llength $connect_ip] != 0} {
-		hsi::utils::add_new_dts_param "$node" "xlnx,snd-pcm" $connect_ip reference
-	} else {
-		dtg_warning "$drv_handle connected ip is NULL for the pin M_AXIS_DATA"
-	}
 }
--- audio_formatter/data/audio_formatter.tcl
+++ audio_formatter/data/audio_formatter.tcl
@@ -27,29 +27,5 @@ proc generate {drv_handle} {
 	set compatible [get_comp_str $drv_handle]
 	set compatible [append compatible " " "xlnx,audio-formatter-1.0"]
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
-	set tx_connect_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "m_axis_mm2s"]
-	foreach connect_ip $tx_connect_ip {
-		if {[llength $connect_ip] != 0} {
-			set connected_ip_type [get_property IP_NAME $connect_ip]
-			if {[string match -nocase $connected_ip_type "system_ila"]} {
-				continue
-			}
-			hsi::utils::add_new_dts_param "$node" "xlnx,tx" $connect_ip reference
-		} else {
-			dtg_warning "$drv_handle pin m_axis_mm2s is not connected... check your design"
-		}
-	}
-	set rx_connect_ip [hsi::utils::get_connected_stream_ip [get_cells -hier $drv_handle] "s_axis_s2mm"]
-	foreach connect_ip $rx_connect_ip {
-		if {[llength $connect_ip] != 0} {
-			set connected_ip_type [get_property IP_NAME $connect_ip]
-			if {[string match -nocase $connected_ip_type "system_ila"]} {
-				continue
-			}
-			hsi::utils::add_new_dts_param "$node" "xlnx,rx" $connect_ip reference
-		} else {
-			dtg_warning "$drv_handle pin s_axis_s2mm is not connected... check your design"
-		}
-	}
 
 }
