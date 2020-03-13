ddcb7566721105c66cdceca7c5d4f3fbeeea67d1
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2020-03-13 00:43:58 -0600

    ai_engine: Add the ai_engine IP interrupts
    Adding the ai_engine interrupts and aie_npi IP.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

--- ai_engine/data/ai_engine.tcl
+++ ai_engine/data/ai_engine.tcl
@@ -27,4 +27,18 @@ proc generate {drv_handle} {
 	set compatible [get_comp_str $drv_handle]
 	set compatible [append compatible " " "xlnx,ai_engine"]
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
+	set intr_names "interrupt1"
+	lappend intr_names "interrupt2"
+	lappend intr_names "interrupt3"
+	set intr_num "0x0 0x94 0x1>, <0x0 0x95 0x1>, <0x0 0x96 0x1"
+	hsi::utils::add_new_dts_param "${node}" "interrupt-names" $intr_names stringlist
+	hsi::utils::add_new_dts_param ${node} "interrupts" $intr_num intlist
+	hsi::utils::add_new_dts_param "${node}" "interrupt-parent" gic reference
+
+	set bus_node "amba_pl"
+	set dts_file [current_dt_tree]
+	set aie_npi_node [add_or_get_dt_node -n "aie-npi" -l aie_npi -u f70a0000 -d $dts_file -p $bus_node]
+	hsi::utils::add_new_dts_param "${aie_npi_node}" "compatible" "xlnx,ai-engine-npi" stringlist
+	set aie_npi_reg "0x0 0xf70a0000 0x0 0x1000"
+	hsi::utils::add_new_dts_param "${aie_npi_node}" "reg" $aie_npi_reg intlist
 }
