f0bc0efee5774b1843e1a2ca20b9fa9c8697637b
Wendy Liang <wendy.liang@xilinx.com>
2020-08-27 14:54:58 -0700

    ai_engine: update for the new AI engine kernel driver
    Update AI engine device node generation for the latest AI engine kernel
    drier change.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    
    Reviewed-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

--- ai_engine/data/ai_engine.tcl
+++ ai_engine/data/ai_engine.tcl
@@ -25,17 +25,25 @@ proc generate {drv_handle} {
 		return
 	}
 	set compatible [get_comp_str $drv_handle]
-	set compatible [append compatible " " "xlnx,ai_engine"]
+	set compatible [append compatible " " "xlnx,ai-engine-v1.0"]
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
 	set intr_names "interrupt1"
-	lappend intr_names "interrupt2"
-	lappend intr_names "interrupt3"
-	set intr_num "0x0 0x94 0x1>, <0x0 0x95 0x1>, <0x0 0x96 0x1"
+	set intr_num "0x0 0x94 0x1"
 	set power_domain "&versal_firmware 0x18224072"
 	hsi::utils::add_new_dts_param "${node}" "interrupt-names" $intr_names stringlist
 	hsi::utils::add_new_dts_param ${node} "interrupts" $intr_num intlist
 	hsi::utils::add_new_dts_param "${node}" "interrupt-parent" gic reference
 	hsi::utils::add_new_dts_param "${node}" "power-domains" $power_domain intlist
+	hsi::utils::add_new_dts_param "${node}" "#address-cells" "2" intlist
+	hsi::utils::add_new_dts_param "${node}" "#size-cells" "2" intlist
+
+	# Add one AI engine partition child node
+	set ai_part_id 0
+	set ai_part_nid 1
+	set ai_part_node [add_or_get_dt_node -n "aie_partition" -u "${ai_part_id}" -l "aie_partition${ai_part_id}" -p ${node}]
+	hsi::utils::add_new_dts_param "${ai_part_node}" "reg" "0 0 50 9" intlist
+	hsi::utils::add_new_dts_param "${ai_part_node}" "xlnx,partition-id" "${ai_part_nid}" intlist
+
 	set dt_overlay [get_property CONFIG.dt_overlay [get_os]]
 	if {$dt_overlay} {
 		set RpRm [hsi::utils::get_rp_rm_for_drv $drv_handle]
@@ -48,8 +56,4 @@ proc generate {drv_handle} {
 	} else {
 		set bus_node "amba_pl"
 	}
-	set aie_npi_node [add_or_get_dt_node -n "aie-npi" -l aie_npi -u f70a0000 -p $bus_node]
-	hsi::utils::add_new_dts_param "${aie_npi_node}" "compatible" "xlnx,ai-engine-npi" stringlist
-	set aie_npi_reg "0x0 0xf70a0000 0x0 0x1000"
-	hsi::utils::add_new_dts_param "${aie_npi_node}" "reg" $aie_npi_reg intlist
 }
