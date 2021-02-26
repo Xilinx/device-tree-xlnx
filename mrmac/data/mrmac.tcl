e5a0e31284d1c02bbdf19b579d2e89fa45da96b8
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2021-02-26 06:04:03 -0700

    mrmac: Remove the hardcoded base address for mrmac cores
    Don't hardcode the base address for mrmac cores and just
    add the base address with offsets 0x0, 0x1000, 0x2000
    and 0x3000.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

--- mrmac/data/mrmac.tcl
+++ mrmac/data/mrmac.tcl
@@ -234,7 +234,6 @@ proc generate {drv_handle} {
 	hsi::utils::add_new_dts_param "${node}" "xlnx,gt-ch0-txprogdiv-freq-source-c1" $GT_CH0_TXPROGDIV_FREQ_SOURCE_C1 string
 
 	set base_addr [string tolower [get_property BASE_VALUE $mem_ranges]]
-	set base_addr 0xa4090000
 	set high_addr [string tolower [get_property HIGH_VALUE $mem_ranges]]
 	set mrmac0_highaddr_hex [format 0x%x [expr $base_addr + 0xFFF]]
 	generate_reg_property $node $base_addr $mrmac0_highaddr_hex
