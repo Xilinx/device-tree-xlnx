339133cbd5cfb09e608fffb4c3cb88fb14d0bd24
Ben Levinsky <ben.levinsky@xilinx.com>
2021-11-23 06:55:39 -0800

    kernel: add node IDs for RPU, OCM, TCM
    Add node IDs for TCM, OCM and RPUs so that these can be managed via kernel
    PM drivers.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    Acked-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

--- device_tree/data/kernel_dtsi/2021.2/include/dt-bindings/power/xlnx-zynqmp-power.h
+++ device_tree/data/kernel_dtsi/2021.2/include/dt-bindings/power/xlnx-zynqmp-power.h
@@ -6,6 +6,17 @@
 #ifndef _DT_BINDINGS_ZYNQMP_POWER_H
 #define _DT_BINDINGS_ZYNQMP_POWER_H
 
+#define		PD_RPU		6
+#define		PD_RPU_0	7
+#define		PD_RPU_1	8
+#define		PD_OCM_0	11
+#define		PD_OCM_1	12
+#define		PD_OCM_2	13
+#define		PD_OCM_3	14
+#define		PD_TCM_0_A	15
+#define		PD_TCM_0_B	16
+#define		PD_TCM_1_A	17
+#define		PD_TCM_1_B	18
 #define		PD_USB_0	22
 #define		PD_USB_1	23
 #define		PD_TTC_0	24
