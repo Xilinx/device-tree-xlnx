88ce2cbef97aeffd17bccd2ebcfc88f879336736
Swagath Gadde <swagath.gadde@xilinx.com>
2022-03-24 20:12:48 +0530

    dt-bindings:sync dt-bindings from u-boot
    This patch will sync the dt-bindings from u-boot tree.
    This sync is done as file to file comparision since we dont have commit tracked.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@xilinx.com>
    Acked-by: Varalaxmi Bingi <Varalaxmi.bingi@xilinx.com>

--- device_tree/data/kernel_dtsi/2022.1/include/dt-bindings/gpio/gpio.h
+++ device_tree/data/kernel_dtsi/2022.1/include/dt-bindings/gpio/gpio.h
@@ -23,14 +23,20 @@
 #define GPIO_LINE_OPEN_DRAIN 4
 
 /*
- *  * Open Drain/Collector is the combination of single-ended open drain interface.
- *   * Open Source/Emitter is the combination of single-ended open source interface.
- *    */
+ * Open Drain/Collector is the combination of single-ended open drain interface.
+ * Open Source/Emitter is the combination of single-ended open source interface.
+ */
 #define GPIO_OPEN_DRAIN (GPIO_SINGLE_ENDED | GPIO_LINE_OPEN_DRAIN)
 #define GPIO_OPEN_SOURCE (GPIO_SINGLE_ENDED | GPIO_LINE_OPEN_SOURCE)
 
-/* Bit 3 express GPIO suspend/resume persistence */
-#define GPIO_SLEEP_MAINTAIN_VALUE 0
-#define GPIO_SLEEP_MAY_LOOSE_VALUE 8
+/* Bit 3 express GPIO suspend/resume and reset persistence */
+#define GPIO_PERSISTENT 0
+#define GPIO_TRANSITORY 8
+
+/* Bit 4 express pull up */
+#define GPIO_PULL_UP 16
+
+/* Bit 5 express pull down */
+#define GPIO_PULL_DOWN 32
 
 #endif
