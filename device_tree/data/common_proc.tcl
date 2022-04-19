ebc7e7f9e7d0157e1a5571d4b190fa0c9d04c351
Varalaxmi Bingi <varalaxmi.bingi@xilinx.com>
2022-04-19 10:27:43 +0530

    common_proc.tcl: updating firmware name when external firmware passed also
    This patch will update the firmware name in partial dtsi
    even if external firmware passed.
    
    Signed-off-by: Varalaxmi Bingi <varalaxmi.bingi@xilinx.com>
    Acked-by: Raju Kumar Pothuraju <raju.kumar-pothuraju@xilinx.com>

--- device_tree/data/common_proc.tcl
+++ device_tree/data/common_proc.tcl
@@ -947,17 +947,13 @@ proc set_drv_def_dts {drv_handle} {
 					}
 				}
 				set hw_name [get_property CONFIG.firmware_name [get_os]]
-				set rprmpartial ""
+				set rprmpartial $hw_name
 				if {![llength $hw_name]} {
 					if {[string match -nocase $proctype "psu_cortexa53"]} {
 						set hw_name [::hsi::get_hw_files -filter "TYPE == partial_bit"]
 					} else {
 						append RmName_prop [get_rm_names $drv_handle] "_" "PDI_FILE"
 						set rprmpartial [file tail [get_property $RmName_prop [hsi::current_hw_design]]]
-						append uid_prop [get_rm_names $drv_handle] "_" "HW_DESIGN_ID"
-						set UID [get_property $uid_prop [hsi::current_hw_design]]
-						append pid_prop [get_rm_names $drv_handle] "_" "HW_PARENT_ID"
-						set PID [get_property $pid_prop [hsi::current_hw_design]]
 
 					}
 					set RpRm1 [get_rp_rm_for_drv $drv_handle]
@@ -976,11 +972,17 @@ proc set_drv_def_dts {drv_handle} {
 				if {[llength $rprmpartial]} {
 					puts "rprmpartial:$rprmpartial"
 					hsi::utils::add_new_dts_param "${child_node2}" "firmware-name" "$rprmpartial" string
-					if {[llength $UID]} {
-						hsi::utils::add_new_dts_param "${child_node2}" "uid" $UID int
-					}
-					if {[llength $PID]} {
-						hsi::utils::add_new_dts_param "${child_node2}" "pid" $PID int
+					if {[string match -nocase $proctype "psv_cortexa72"]} {
+						append uid_prop [get_rm_names $drv_handle] "_" "HW_DESIGN_ID"
+						set UID [get_property $uid_prop [hsi::current_hw_design]]
+						append pid_prop [get_rm_names $drv_handle] "_" "HW_PARENT_ID"
+						set PID [get_property $pid_prop [hsi::current_hw_design]]
+						if {[llength $UID]} {
+							hsi::utils::add_new_dts_param "${child_node2}" "uid" $UID int
+						}
+						if {[llength $PID]} {
+							hsi::utils::add_new_dts_param "${child_node2}" "pid" $PID int
+						}
 					}
 
 				}
