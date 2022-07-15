b2677a2157a3016af89219bcb0b8aebe00c6d2b7
Varalaxmi Bingi <varalaxmi.bingi@xilinx.com>
2022-07-15 11:16:07 +0530

    xlnx-versal-net-power.h: adding power header file
    This patch will add power header file for versal-net.
    
    Signed-off-by: Varalaxmi Bingi <varalaxmi.bingi@xilinx.com>
    
    Acked-for-series: Raju Kumar Pothuraju <raju.kumar-pothuraju@xilinx.com>

---
+++ device_tree/data/kernel_dtsi/2022.2/include/dt-bindings/power/xlnx-versal-net-power.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Copyright (C) 2022 Xilinx, Inc.
+ */
+
+#ifndef _DT_BINDINGS_VERSAL_NET_POWER_H
+#define _DT_BINDINGS_VERSAL_NET_POWER_H
+
+#define PM_DEV_USB_0				(0X1821C032U)
+#define PM_DEV_USB_1				(0X1821C033U)
+#define PM_DEV_SPI_0				(0X1821C02DU)
+#define PM_DEV_SPI_1				(0X1821C02EU)
+#define PM_DEV_UART_0				(0X1821C02BU)
+#define PM_DEV_UART_1				(0X1821C02CU)
+#define PM_DEV_OSPI				(0X1821C030U)
+#define PM_DEV_QSPI				(0X1821C02FU)
+#define PM_DEV_SDIO_0				(0X1821C031U)
+#define PM_DEV_SDIO_1				(0X1821C03EU)
+#define PM_RST_USB0				(0XC104079U)
+#define PM_RST_USB1				(0XC10407AU)
+
+#endif
