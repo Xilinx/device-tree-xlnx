be97a6c046ca2069dfe6b9ac2bc1724cfba7e0bc
Bingi, Varalaxmi <varalaxmi.bingi@amd.com>
2023-03-06 15:03:23 +0530

    scugic.tcl:enabling gic_its node status
    gic_its node status was disabled default in board dtsi file.
    This should be enabled if gic_its available.
    To enable the status there is no hsi property earlier.
    Now HSI team added a property based on that enabling the status
    and checking for cpm enablement also as this is only required
    for cpm in versal.
    
    Signed-off-by: Varalaxmi Bingi <varalaxmi.bingi@amd.com>
    
    Acked-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>

--- scugic/data/scugic.tcl
+++ scugic/data/scugic.tcl
@@ -15,4 +15,20 @@
 
 proc generate {drv_handle} {
 
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+	set default_dts [set_drv_def_dts $drv_handle]
+	set proctype [get_property IP_NAME [get_cells -hier [get_sw_processor]]]
+	set cpm_ip [get_cells -hier -filter IP_NAME==psv_cpm]
+	if {[string match -nocase $proctype "psv_cortexa72"] && \
+		[string match -nocase [get_property CONFIG.APU_GIC_ITS_CTL [get_cells -hier $drv_handle]] "0xF9020000"] && \
+		[llength $cpm_ip]} {
+		set gic_node [add_or_get_dt_node -n "&gic_its" -d $default_dts]
+		hsi::utils::add_new_dts_param "${gic_node}" "status" "okay" string
+	}
 }
