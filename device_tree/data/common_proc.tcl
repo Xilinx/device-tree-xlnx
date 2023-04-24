e834e387d7fda1a627d520d201b85e3f89197e56
Bingi, Varalaxmi <varalaxmi.bingi@amd.com>
2023-04-24 19:41:38 +0530

    common_proc.tcl:update generate_reg_property function
    Update the generate_reg_property function to work for
    mrmac, axi_ethernet and other nodes as well.
    
    Signed-off-by: Varalaxmi Bingi <varalaxmi.bingi@amd.com>
    
    Acked-for-series: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>

--- device_tree/data/common_proc.tcl
+++ device_tree/data/common_proc.tcl
@@ -7314,7 +7314,6 @@ proc generate_cci_node { drv_handle rt_node} {
 #function.
 proc generate_reg_property {base high} {
 	set size [format 0x%x [expr {${high} - ${base} + 1}]]
-
 	set proctype [get_property IP_NAME [get_cells -hier [get_sw_processor]]]
 	if {[string match -nocase $proctype "psu_cortexa53"] || [string match -nocase $proctype "psv_cortexa72"] || [string match -nocase $proctype "psx_cortexa78"]} {
 		if {[regexp -nocase {0x([0-9a-f]{9})} "$base" match]} {
@@ -7325,25 +7324,23 @@ proc generate_reg_property {base high} {
 			set high_base "0x[string range $temp $rem $len]"
 			set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
 			set low_base [format 0x%08x $low_base]
+			if {[regexp -nocase {0x([0-9a-f]{9})} "$size" match]} {
+				set temp $size
+				set temp [string trimleft [string trimleft $temp 0] x]
+				set len [string length $temp]
+				set rem [expr {${len} - 8}]
+				set high_size "0x[string range $temp $rem $len]"
+				set low_size  "0x[string range $temp 0 [expr {${rem} - 1}]]"
+				set low_size [format 0x%08x $low_size]
+				set reg "$low_base $high_base $low_size $high_size"
+			} else {
+				set reg "$low_base $high_base 0x0 $size"
+			}
 		} else {
-			set high_base $base
-			set low_base 0x0
+			set reg "0x0 $base 0x0 $size"
 		}
-		if {[regexp -nocase {0x([0-9a-f]{9})} "$size" match]} {
-			set temp $size
-			set temp [string trimleft [string trimleft $temp 0] x]
-			set len [string length $temp]
-			set rem [expr {${len} - 8}]
-			set high_size "0x[string range $temp $rem $len]"
-			set low_size  "0x[string range $temp 0 [expr {${rem} - 1}]]"
-			set low_size [format 0x%08x $low_size]
-		} else {
-			set high_size $size
-			set low_size 0x0
-		}
-		set reg "$low_base $high_base $low_size $high_size"
 	} else {
-		set reg "0x0 $base 0x0 $size"
+		set reg "$base $size"
 	}
 	return $reg
 }
