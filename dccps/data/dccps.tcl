8aa417b2b9aa9d170e1cbc7aeaebe6e1cde5b469
Lomate, Ashwini <ashwini.lomate@amd.com>
2024-03-19 18:55:26 -1200

    dccps.mdd:dccps.tcl: Add support for coresight dcc ip in tcl
    Add support for coresight dcc ip in tcl
    
    Signed-off-by: Ashwini Lomate <ashwini.lomate@amd.com>
    
    Acked-for-series: Varalaxmi Bingi <Varalaxmi.bingi@amd.com>

---
+++ dccps/data/dccps.tcl
@@ -0,0 +1,30 @@
+#
+# (C) Copyright 2020-2022 Xilinx, Inc.
+# (C) Copyright 2022 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	# try to source the common tcl procs
+	# assuming the order of return is based on repo priority
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+	set ip [get_cells -hier $drv_handle]
+	set def_dts [get_property CONFIG.pcw_dts [get_os]]
+	set dcc_node [add_or_get_dt_node -n "&dcc" -d $def_dts]
+	hsi::utils::add_new_dts_param "${dcc_node}" "status" "okay" string
+}
