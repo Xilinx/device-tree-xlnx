44e1a9851addfc26886d0ec479c206a58d666f6d
Lomate, Ashwini <ashwini.lomate@amd.com>
2024-10-23 18:42:35 -1200

    Revert "mrmac.tcl: Fix for DTG error while using the 2024.2 L20 MRMAC design"
    This reverts commit 359f7b152a8486886324ffca510cf893da7052ba.
    Acked-by: Varalaxmi Bingi <Varalaxmi.bingi@amd.com>

--- mrmac/data/mrmac.tcl
+++ mrmac/data/mrmac.tcl
@@ -1637,17 +1637,11 @@ proc generate_intr_info {drv_handle node fifo_ip} {
 		}
 	}
 	set intr_val [get_property CONFIG.interrupts $target_handle]
-	if {[llength $intr_val]} {
-		hsi::utils::add_new_dts_param "${node}" "interrupts" $intr_val int
-	}
 	set intr_parent [get_property CONFIG.interrupt-parent $target_handle]
-	if {[llength $intr_parent]} {
-		hsi::utils::add_new_dts_param "${node}" "interrupt-parent" $intr_parent reference
-	}
 	set int_names  [get_property CONFIG.interrupt-names $target_handle]
-	if {[llength $int_names]} {
-		hsi::utils::add_new_dts_param "${node}" "interrupt-names" $int_names stringlist
-	}
+	hsi::utils::add_new_dts_param "${node}" "interrupts" $intr_val int
+	hsi::utils::add_new_dts_param "${node}" "interrupt-parent" $intr_parent reference
+	hsi::utils::add_new_dts_param "${node}" "interrupt-names" $int_names stringlist
 }
 
 proc check_size {base node} {
