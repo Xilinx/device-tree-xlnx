0a92c9ca41a43a3d3293ccc915b819294e61612c
Luke Millar <134084040+GrizzMillar@users.noreply.github.com>
2025-01-08 10:06:13 +0000

    Update input.h
    update include line

--- device_tree/data/kernel_dtsi/2024.2/include/dt-bindings/input/input.h
+++ device_tree/data/kernel_dtsi/2024.2/include/dt-bindings/input/input.h
@@ -9,7 +9,7 @@
 #ifndef _DT_BINDINGS_INPUT_INPUT_H
 #define _DT_BINDINGS_INPUT_INPUT_H
 
-#include <dt-bindings/input/linux-event-codes.h>
+#include "linux-event-codes.h"
 #define MATRIX_KEY(row, col, code)	\
 	((((row) & 0xFF) << 24) | (((col) & 0xFF) << 16) | ((code) & 0xFFFF))
 
