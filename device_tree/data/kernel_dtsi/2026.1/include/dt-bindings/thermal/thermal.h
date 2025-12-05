613747e1f1ce845afe4862585b8ceb0ed9d6df43
Altaf Patel <Patel.AltafAbedali@amd.com>
2025-12-05 10:25:15 +0530

    device_tree: Add 2026.1 support in DTG
    Enable 2026.1 in mld file and copy dtsi files.
    
    Signed-off-by: Altaf Patel <Patel.AltafAbedali@amd.com>
    
    Acked-by : Ashwini Lomate <ashwini.lomate@amd.com>

---
+++ device_tree/data/kernel_dtsi/2026.1/include/dt-bindings/thermal/thermal.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * This header provides constants for most thermal bindings.
+ *
+ * Copyright (C) 2013 Texas Instruments
+ * (C) Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+ *	Eduardo Valentin <eduardo.valentin@ti.com>
+ */
+
+#ifndef _DT_BINDINGS_THERMAL_THERMAL_H
+#define _DT_BINDINGS_THERMAL_THERMAL_H
+
+/* On cooling devices upper and lower limits */
+#define THERMAL_NO_LIMIT		(~0)
+
+#endif
+
